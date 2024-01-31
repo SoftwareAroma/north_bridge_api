d8b95c7dae3481e9f06843c17479db1eaf53bfd7
Software Aroma <softwarearomagh@gmail.com>
2024-01-31 19:43:38 +0000

    implementing payment gateway

--- src/payment/payment.controller.ts
+++ src/payment/payment.controller.ts
@@ -1,4 +1,50 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Get, Post, UseGuards } from '@nestjs/common';
+import { PaymentService } from './payment.service';
+import { CreatePaymentDto } from './dto/create.dto';
+import { ApiResponse } from '@nestjs/swagger';
+import { JwtAuthGuard } from '@shared/guard';
 
 @Controller('payment')
-export class PaymentController {}
+export class PaymentController {
+    constructor(
+        private readonly paymentService: PaymentService,
+    ) { }
+
+    @UseGuards(JwtAuthGuard)
+    @Post('initialize')
+    @ApiResponse({
+        status: 200,
+        description: 'Get all transactions',
+        type: Object,
+    })
+    async createPayment(
+        @Body() body: CreatePaymentDto,
+    ) {
+        return this.paymentService.createPayment(body);
+    }
+
+    @UseGuards(JwtAuthGuard)
+    @Post('verify')
+    @ApiResponse({
+        status: 200,
+        description: 'Get all transactions',
+        type: Object,
+    })
+    async verifyPayment(
+        @Body() body: { reference: string },
+    ) {
+        return this.paymentService.verifyPayment(body.reference);
+    }
+
+    @UseGuards(JwtAuthGuard)
+    @Get('transactions')
+    @ApiResponse({
+        status: 200,
+        description: 'Get all transactions',
+        type: Object,
+    })
+    async getTransactions() {
+        return this.paymentService.transactions();
+    }
+
+}
--- src/payment/payment.module.ts
+++ src/payment/payment.module.ts
@@ -1,9 +1,22 @@
 import { Module } from '@nestjs/common';
 import { PaymentService } from './payment.service';
 import { PaymentController } from './payment.controller';
+import { HttpModule } from '@nestjs/axios';
+import { CaslModule } from '@shared/casl/casl.module';
+import { PassportModule } from '@nestjs/passport';
+import { JwtModule } from '@nestjs/jwt';
+import { jwtConstants } from '@shared';
 
 @Module({
-  imports: [],
+  imports: [
+    HttpModule,
+    CaslModule,
+    PassportModule.register({ defaultStrategy: 'jwt' }),
+    JwtModule.register({
+      secret: jwtConstants.secret,
+      signOptions: { expiresIn: jwtConstants.expiresIn },
+    }),
+  ],
   providers: [PaymentService],
   controllers: [PaymentController],
   exports: [PaymentService],
